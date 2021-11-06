0c3551760ace6efe553537ff67301066c5672f4b
Maxhunter007 <zang.maximilian@gmail.com>
2021-11-06 01:11:49 +0100

    added basic HUD and PixelperfectCamera

---
+++ SwarmGame/Assets/Scripts/HUDManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour
+{
+    private bool initialized = false;
+    
+    private Text pollenAmount = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeReferences();
+        initialized = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void InitializeReferences()
+    {
+        pollenAmount = gameObject.GetComponentInChildren<Text>();
+    }
+
+    public bool GetInitialized()
+    {
+        return initialized;
+    }
+
+    public void UpdatePollenAmount(int newPollen)
+    {
+        pollenAmount.text = "Pollen: " + newPollen;
+    }
+}
---
+++ SwarmGame/Assets/Scripts/RessourcenManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RessourcenManager : MonoBehaviour
+{
+    private int pollen;
+    private readonly int startingPollen = 100;
+
+    [SerializeField] private HUDManager Hud = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //InitializeReferences();
+        InitializeResources();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void InitializeResources()
+    {
+        pollen = startingPollen;
+        UpdateUI();
+    }
+
+    public int GetPollen()
+    {
+        return pollen;
+    }
+
+    public void AddPollen(int addedPollen)
+    {
+        pollen += addedPollen;
+        UpdateUI();
+    }
+    
+    private void InitializeReferences()
+    {
+        
+    }
+
+    private void UpdateUI()
+    {
+        if (Hud.GetInitialized())
+        {
+            Hud.UpdatePollenAmount(pollen);
+        }
+    }
+}
