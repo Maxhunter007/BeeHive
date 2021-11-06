19155c93204a19f703fd043e17df8fdf4b7636c7
Daniel Eckmann <daniel.eckmann@stud-mail.uni-wuerzburg.de>
2021-11-06 10:44:20 +0100

    Bee and Tree textures added and adjustments to swarm behaviour

--- SwarmGame/Assets/Scripts/BoidLeader.cs
+++ SwarmGame/Assets/Scripts/BoidLeader.cs
@@ -7,6 +7,7 @@ public class BoidLeader : MonoBehaviour
 {
     private Rigidbody rb;
     public float speed = 5.0f;
+    public float maxVelocity = 10.0f;
     
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,26 @@ public class BoidLeader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = Vector3.forward * speed;
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector3.down);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right);
+        }
+
+        if (rb.velocity.magnitude > maxVelocity)
+        {
+            rb.velocity = Vector3.Normalize(rb.velocity) * maxVelocity;
+        }
     }
 }
